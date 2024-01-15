f50c66104517ec513f28e73aadfe4c5daf1cab31
NQTruong02 <nguyentruongbg2002@gmail.com>
2024-01-15 13:56:28 +0700

    okkkk

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import NavBar from "../components/NavBar";
 export const AppRoutes: React.FC = () => {
   const commonRoutes = [{ path: "/", element: <ListLayout /> }];
   const Home = [{ path: "/home", element: <ListLayout /> }];
-
+//hehe
   return (
     <>
       <NavBar />
