9de4ea1eddff791dfe4e83852d978c98f2071d41
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-01-15 09:54:09 +0700

    ok

--- src/features/products/components/listLayout.tsx
+++ src/features/products/components/listLayout.tsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 import { GetAllProduct, Product } from "../api/getAllProduct";
 
-
 export const ListLayout = () => {
-    const [data, setData] = useState<Product[]>([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await GetAllProduct();
-            setData(result);
-        };
+  const [data, setData] = useState<Product[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await GetAllProduct();
+      setData(result);
+    };
 
-        fetchData();
-    }, []);
-    return(
-        <div>
-            <h1>hehe</h1>
-           <div>
-            {data.map((product, index) => (
-                <p key={index}>{product.productName}</p>
-            ))}
-        </div>
-        </div>
-      
-    );
-}
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>hehe</h1>
+      <div>
+        {data.map((product, index) => (
+          <div>
+            <p key={index}>{product.productName}</p>
+            <p key={index}>{product.productName}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
