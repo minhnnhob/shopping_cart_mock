117454abfa7ddc17d2f90c930390a66121fc0279
gikhanh <giakhanh1310@gmai.com>
2024-01-18 12:49:40 +0700

    Refactor ListCart component delete da chay

--- src/features/cart/componets/listCart.tsx
+++ src/features/cart/componets/listCart.tsx
@@ -1,19 +1,17 @@
-// import { useState } from "react";
-// import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import {
-  StyledRow,
-  StyledCol,
-  StyledCard,
-  StyledCardBody,
-  ImageContainer,
-  StyledImg,
-  DetailsContainer,
-  StyledTitle,
-  StyledText,
-  QuantityContainer,
-  // QuantityButton,
-  PriceContainer,
-  // StyledFontAwesomeIcon,
+    StyledRow,
+    StyledCol,
+    StyledCard,
+    StyledCardBody,
+    ImageContainer,
+    StyledImg,
+    DetailsContainer,
+    StyledTitle,
+    StyledText,
+    QuantityContainer,
+    PriceContainer,
+    StyledFontAwesomeIcon,
 } from "./Cart.styled";
 
 import { useSelector } from "react-redux";
@@ -21,68 +19,44 @@ import { RootState } from "../../../stores/store";
 import { useCartActions } from "../api/index";
 
 const ListCart = () => {
-  const { handleRemove } = useCartActions(); // use the custom hook
-  const cartItems = useSelector((state: RootState) => state.cart.items);
+    const { handleRemove } = useCartActions(); // use the custom hook
+    const cartItems = useSelector((state: RootState) => state.cart.items);
 
-  const handleRemoveItem = (itemId: string) => {
-    handleRemove(itemId);
-  };
+    const handleRemoveItem = (itemId: string) => {
+        handleRemove(itemId);
+    };
 
-  // const handleIncrement = (itemId: number) => {
-  //   const updatedCart = cartItems.map((item) =>
-  //     item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
-  //   );
-  //   setCartItems(updatedCart);
-  // };
-
-  // const handleDecrement = (itemId: number) => {
-  //   const updatedCart = cartItems.map((item) =>
-  //     item.id === itemId && item.quantity > 1
-  //       ? { ...item, quantity: item.quantity - 1 }
-  //       : item
-  //   );
-  //   setCartItems(updatedCart);
-  // };
-
-  if (cartItems.length === 0) {
-    return <p>There are 0 products in the cart.</p>;
-  }
-  return (
-    <StyledRow>
-      {cartItems.map((item) => (
-        <StyledCol key={item.productId} md={4}>
-          <StyledCard>
-            <StyledCardBody>
-              <ImageContainer>
-                <StyledImg src={item.imageUrl} />
-                <DetailsContainer>
-                  <StyledTitle>{item.imageUrl}</StyledTitle>
-                  <StyledText>{item.description}</StyledText>
-                  <QuantityContainer>
-                    {/* <QuantityButton onClick={() => handleDecrement(item.productId)}>
-                      -
-                    </QuantityButton> */}
-                    {/* <span>{item.quantity}</span> */}
-                    {/* <QuantityButton onClick={() => handleIncrement(item.productId)}>
-                      +
-                    </QuantityButton> */}
-                  </QuantityContainer>
-                </DetailsContainer>
-              </ImageContainer>
-              <PriceContainer>
-                {/* <StyledFontAwesomeIcon
-                  icon={faTrashAlt}
-                  onClick={() => handleRemoveItem(item.id)}
-                /> */}
-                <StyledText>${item.price}</StyledText>
-              </PriceContainer>
-            </StyledCardBody>
-          </StyledCard>
-        </StyledCol>
-      ))}
-    </StyledRow>
-  );
+    if (cartItems.length === 0) {
+        return <p>There are 0 products in the cart.</p>;
+    }
+    return (
+        <StyledRow>
+            {cartItems.map((item) => (
+                <StyledCol key={item.productId} md={4}>
+                    <StyledCard>
+                        <StyledCardBody>
+                            <ImageContainer>
+                                <StyledImg src={item.imageUrl} />
+                                <DetailsContainer>
+                                    <StyledTitle>{item.imageUrl}</StyledTitle>
+                                    <StyledText>{item.description}</StyledText>
+                                    <QuantityContainer>
+                                    </QuantityContainer>
+                                </DetailsContainer>
+                            </ImageContainer>
+                            <PriceContainer>
+                                <StyledFontAwesomeIcon
+                                    icon={faTrashAlt}
+                                    onClick={() => handleRemoveItem(item.productId)}
+                                />
+                                <StyledText>${item.price}</StyledText>
+                            </PriceContainer>
+                        </StyledCardBody>
+                    </StyledCard>
+                </StyledCol>
+            ))}
+        </StyledRow>
+    );
 };
-//hehe
 
 export default ListCart;
